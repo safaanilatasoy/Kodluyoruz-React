236a35470c1430dde05825feec3ff182b418459d
safaanilatasoy <safaanilatasoy@gmail.com>
2023-12-01 22:53:05 +0300

    Koşullu Render İşlemi

--- components/hello-react/src/App.js
+++ components/hello-react/src/App.js
@@ -4,27 +4,29 @@ import Header from './components/Header'
 
 const name = "Anıl";
 const surname = "Atasoy";
+const isLoggedIn = true;
 
 function App() {
   return (
     <>
       <Header />
-      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. 
-        Ipsa aliquam totam, quam vero aperiam non? Commodi, est 
-        adipisci ipsa nam nostrum magni dolorum repudiandae velit. 
-        Obcaecati.</p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa aliquam
+        totam, quam vero aperiam non? Commodi, est adipisci ipsa nam nostrum
+        magni dolorum repudiandae velit. Obcaecati.
+      </p>
 
-        <label htmlFor="myInput">
-          Name:
-         
-          <input id="myInput" type="text" />
-        </label>
+      <label htmlFor="myInput">
+        Name:
+        <input id="myInput" type="text" />
+      </label>
 
-        <h3>{name}</h3>
-        <h3>{`Fullname: ${name} ${surname}`}</h3>
+      {/* if login success*/}
+      <h3 className="loginSuccess">{isLoggedIn && `Hoşgeldiniz ${name} ${surname}`}</h3>
 
+      {/* if login fail */}
+      <h3 className="loginFail">{!isLoggedIn && `Lütfen giriş yapınız`}</h3>
     </>
-
   );
 }
 

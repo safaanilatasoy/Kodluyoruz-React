c25adad620bee85860a118f8a1eafbcc39e1ab93
safaanilatasoy <safaanilatasoy@gmail.com>
2023-12-01 22:36:48 +0300

    JSX yapısı

--- components/hello-react/src/App.js
+++ components/hello-react/src/App.js
@@ -3,9 +3,21 @@ import Header from './components/Header'
 
 function App() {
   return (
-    <div className="App">
+    <>
       <Header />
-    </div>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+        Ipsa aliquam totam, quam vero aperiam non? Commodi, est 
+        adipisci ipsa nam nostrum magni dolorum repudiandae velit. 
+        Obcaecati.</p>
+
+        <label htmlFor="myInput">
+          Name:
+         
+          <input id="myInput" type="text" />
+        </label>
+
+    </>
+
   );
 }
 

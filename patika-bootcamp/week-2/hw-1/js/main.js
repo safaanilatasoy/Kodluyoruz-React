0cbf55c5ba98a799304171e256fd5ad22795518b
safaanilatasoy <safaanilatasoy@gmail.com>
2023-12-03 21:37:50 +0300

    Page theme, fetch data and general codes are completed

---
+++ patika-bootcamp/week-2/hw-1/js/main.js
@@ -0,0 +1,26 @@
+const url = "https://jsonplaceholder.typicode.com/todos?_limit=5";
+
+fetch(url)
+    .then(res => res.json())
+    .then(data => {
+
+        const dataList = document.querySelector(".dataList");
+
+        data.map((user) => {
+            const tr = document.createElement("tr");
+
+            tr.innerHTML = `
+                        <th scope="row">${user.id}</th>
+                        <td>${user.title}</td>
+                        <td>${
+                          user.completed
+                            ? '<i class="fa-solid fa-check" style="color: #3cbe19;"></i>'
+                            : '<i class="fa-solid fa-xmark" style="color: #cb1010;"></i>'
+                        }</td>
+                    `;
+
+            dataList.appendChild(tr);
+
+        })
+    });
+

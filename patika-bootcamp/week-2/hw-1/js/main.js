aa5945f1faa50220a2919d0fa479348788edfd4f
safaanilatasoy <safaanilatasoy@gmail.com>
2023-12-03 21:44:50 +0300

    added explanatory comment lines to the code

--- patika-bootcamp/week-2/hw-1/js/main.js
+++ patika-bootcamp/week-2/hw-1/js/main.js
@@ -1,5 +1,9 @@
+
+// Data API URL
 const url = "https://jsonplaceholder.typicode.com/todos?_limit=5";
 
+
+// Fetching Data
 fetch(url)
     .then(res => res.json())
     .then(data => {
@@ -12,11 +16,12 @@ fetch(url)
             tr.innerHTML = `
                         <th scope="row">${user.id}</th>
                         <td>${user.title}</td>
-                        <td>${
-                          user.completed
+                        <td>
+                        ${user.completed
                             ? '<i class="fa-solid fa-check" style="color: #3cbe19;"></i>'
                             : '<i class="fa-solid fa-xmark" style="color: #cb1010;"></i>'
-                        }</td>
+                        }
+                        </td>
                     `;
 
             dataList.appendChild(tr);

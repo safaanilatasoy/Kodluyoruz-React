11a4b00ada0b40838c6de4f4cd04940cfa4500aa
safaanilatasoy <safaanilatasoy@gmail.com>
2023-12-04 21:36:52 +0300

    Array States

--- components/state-app/src/App.js
+++ components/state-app/src/App.js
@@ -6,6 +6,7 @@ import {useState} from "react";
 function App() {
 const [name, setName] = useState('Anil');
 const [age, setAge] = useState(25);
+const [friends, setFriends] = useState(["Ahmet","Ali", "Murat"]);
 
   return (
     <div className="App">
@@ -13,6 +14,14 @@ const [age, setAge] = useState(25);
       <h2>{age}</h2>
       <button onClick={() => setName("Ahmet")}>Change Name</button>
       <button onClick={() => setAge(18)}>Change Age</button>
+
+      <hr />
+      <h2>Friends</h2>
+      {friends.map((friend, index) => (
+        <div key="index">{friend}</div>
+      ))}
+
+      <button onClick={() => setFriends([...friends,"Ayşe"])}>Add new friend</button>
     </div>
   );
 }

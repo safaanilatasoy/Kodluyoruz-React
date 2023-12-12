12be8f7ab56927c6938db69c089cc4b37e3ef23b
safaanilatasoy <safaanilatasoy@gmail.com>
2023-12-12 15:33:57 +0300

    main

--- components/contact-app/src/components/Contacts/Form/index.js
+++ components/contact-app/src/components/Contacts/Form/index.js
@@ -1,9 +1,48 @@
-import React from 'react'
+import {useState} from 'react'
 
-function Form() {
+function Form({addContact, contacts}) {
+  const [form, setForm] = useState({fullname: "", phoneNumber: ""});
+
+  const onChangeInput = (e) => {
+    setForm({...form, [e.target.name]: e.target.value});
+  }
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+
+    if (form.fullname === "" || form.phoneNumber === "") {
+      return false;
+    }
+
+    addContact([...contacts, form])
+
+    setForm({fullname: "", phoneNumber: ""});
+  }
+
   return (
     <div>
-      Form
+      <form onSubmit={onSubmit}>
+      <div>
+        <input 
+        name="fullname" 
+        placeholder='Full Name'  
+        onChange={onChangeInput}
+        value={form.fullname}
+        />
+      </div>
+      <div>
+        <input 
+          name="phoneNumber" 
+          placeholder='Phone Number' 
+          onChange={onChangeInput}
+          value={form.phoneNumber}
+        />
+      </div>
+
+      <div>
+        <button>Add</button>
+      </div>
+      </form>
     </div>
   )
 }

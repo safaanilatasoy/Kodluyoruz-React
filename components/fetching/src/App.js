09b9f555aaaa19b8543c80578a5cb499ae179761
safaanilatasoy <safaanilatasoy@gmail.com>
2023-12-17 17:50:34 +0300

    axios

--- components/fetching/src/App.js
+++ components/fetching/src/App.js
@@ -1,10 +1,10 @@
 import './App.css';
-import Users from './components/Users';
+import UsersAxios from './components/UsersAxios';
 
 function App() {
   return (
     <div className="App">
-      <Users />
+      <UsersAxios />
     </div>
   );
 }

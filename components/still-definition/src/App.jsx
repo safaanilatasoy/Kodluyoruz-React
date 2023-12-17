e705b7004125612a6843d9331dc8b996fe5c0ff9
safaanilatasoy <safaanilatasoy@gmail.com>
2023-12-17 15:55:07 +0300

    Stil Tanımı Yapmak || React + Vite Creating app

---
+++ components/still-definition/src/App.jsx
@@ -0,0 +1,18 @@
+
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div style={{color: '#fff', backgroundColor: 'blue', padding: '10px'}}>
+        Lorem ipsum dolor sit amet consectetur, 
+        adipisicing elit. Quidem, labore.
+      </div>
+      
+      
+    </>
+  )
+}
+
+export default App

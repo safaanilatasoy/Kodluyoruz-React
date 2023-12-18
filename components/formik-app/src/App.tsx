7d8bcdf987f5559e5e1f961182053274016a16b9
safaanilatasoy <safaanilatasoy@gmail.com>
2023-12-18 20:59:58 +0300

    Formik || Radio, Checkbox, Dropdown

--- components/formik-app/src/App.tsx
+++ components/formik-app/src/App.tsx
@@ -1,46 +1,97 @@
-import './App.css';
-import { Formik } from 'formik';
+import "./App.css";
+import { Formik } from "formik";
 
 function App() {
-
   return (
     <>
       <Formik
-      initialValues={{
-        firstName: '',
-        lastName: '',
-        email: '',
-      }}
-      onSubmit={(values) => {
-        console.log(values);
-      }}
-    >
-      {
-        ({handleSubmit, handleChange}) => (
+        initialValues={{
+          firstName: "Anil",
+          lastName: "Atasoy",
+          gender: "male",
+          hobbies: [],
+          country: "turkey",
+        }}
+        onSubmit={(values) => {
+          console.log(values);
+        }}
+      >
+        {({ handleSubmit, handleChange, values }) => (
           <form onSubmit={handleSubmit}>
-        <label htmlFor="firstName">First Name</label>
-        <input name="firstName" onChange={handleChange} placeholder="First Name"/>
+            <label htmlFor="firstName">First Name</label>
+            <input
+              name="firstName"
+              value={values.firstName}
+              onChange={handleChange}
+              placeholder="First Name"
+            />
 
-        <br />
-        <label htmlFor="lastName">Last Name</label>
-        <input name="lastName" onChange={handleChange} placeholder="Last Name"/>
-        <br />
+            <br />
+            <label htmlFor="lastName">Last Name</label>
+            <input
+              name="lastName"
+              value={values.lastName}
+              onChange={handleChange}
+              placeholder="Last Name"
+            />
 
-        <label htmlFor="email">Email</label>
-        <input
-          name="email"
-          placeholder="test@email.com"
-          type="email"
-          onChange={handleChange}
-        />
-        <br />
-        <button type="submit">Submit</button>
-      </form>
-        )
-      }
-    </Formik>
+            <br />
+            <br />
+
+            <span>Male</span>
+            <input
+              name="gender"
+              type="radio"
+              value="male"
+              onChange={handleChange}
+              checked={values.gender === "male"}
+            />
+            <span>Female</span>
+            <input
+              name="gender"
+              type="radio"
+              value="female"
+              onChange={handleChange}
+              checked={values.gender === "female"}
+            />
+
+            <br />
+            <br />
+
+            <div>
+              Football
+              <input type="checkbox" name="hobbies" value="Football"  onChange={handleChange}/>
+            </div>
+            <div>
+              Cinema
+              <input type="checkbox" name="hobbies" value="Cinema"  onChange={handleChange}/>
+            </div>
+            <div>
+              Photography
+              <input type="checkbox" name="hobbies" value="Photography"  onChange={handleChange}/>
+            </div>
+
+            <br /><br />
+
+            <select name="country" value={values.country} onChange={handleChange}>
+              <option value="turkey">Turkey</option>
+              <option value="england">United Kingdom</option>
+              <option value="usa">USA</option>
+            </select>
+
+
+            <br />
+            <br />
+            <button type="submit">Submit</button>
+
+            <br />
+            <br />
+            <code> {JSON.stringify(values)}</code>
+          </form>
+        )}
+      </Formik>
     </>
-  )
+  );
 }
 
-export default App
+export default App;

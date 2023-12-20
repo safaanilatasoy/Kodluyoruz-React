13d6ccb5a04ac04509171487caab384dd016a7f9
safaanilatasoy <safaanilatasoy@gmail.com>
2023-12-20 20:48:48 +0300

    main

---
+++ components/realtime-colors/client/src/components/Palette.js
@@ -0,0 +1,19 @@
+import {useState} from 'react';
+import {send} from "../socketApi";
+
+function Palette({ activeColor }) {
+  const [color, setColor] = useState("");
+
+  return (
+    <div className="palette">
+      <input
+        type="color"
+        value={color}
+        onChange={(e) => setColor(e.target.value)}
+      />
+      <button onClick={() => send(color)}>Click</button>
+    </div>
+  );
+}
+
+export default Palette
---
+++ components/realtime-colors/client/src/socketApi.js
@@ -0,0 +1,25 @@
+import io from "socket.io-client";
+
+let socket;
+
+export const init = () => {
+    console.log("Sunucuya Bağlanılıyor...");
+    socket = io('http://localhost:3001',{
+        transports: ['websocket'],
+    });
+
+
+    socket.on('connect', () => console.log("Sunucuya bağlantı gerçekleşti."));
+}
+
+
+export const send = (color) => {
+    socket.emit('newColor', color);
+}
+
+export const subscribe = (cb) => {
+    socket.on('receive',(color) => {
+        console.log(color);
+        cb(color);
+    })
+}

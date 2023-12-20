446c4ad2ce504a588f00a4d2359190e4a7f88913
safaanilatasoy <safaanilatasoy@gmail.com>
2023-12-20 23:33:15 +0300

    socker.io-client

--- components/chat-app/client/src/components/ChatItem.jsx
+++ components/chat-app/client/src/components/ChatItem.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable react/prop-types */
 import styles from './styles.module.css';
 
 function ChatItem({item}) {
--- components/chat-app/client/src/components/Container.jsx
+++ components/chat-app/client/src/components/Container.jsx
@@ -1,8 +1,15 @@
 
+import {useEffect} from "react";
 import ChatList from "./ChatList";
 import ChatForm from "./ChatForm";
 
+import {init} from '../socketApi';
+
 function Container() {
+
+  useEffect(() => {
+    init();
+  }, []);
   return (
     <div className="App">
       <ChatList />

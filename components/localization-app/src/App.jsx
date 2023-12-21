7ead26923726a60df50fe2c35187d14fdb5c9cb3
safaanilatasoy <safaanilatasoy@gmail.com>
2023-12-21 16:19:05 +0300

    React intle

---
+++ components/localization-app/src/App.jsx
@@ -0,0 +1,37 @@
+/* eslint-disable no-unused-vars */
+import './App.css';
+import { IntlProvider, FormattedMessage, FormattedNumber } from "react-intl";
+import { useState } from 'react';
+
+const messages = {
+  "tr-TR": {
+    title: "Bu bir başlık",
+    description: "3 yeni mesajınız var",
+  },
+  "en-US": {
+    title: "This is a title",
+    description: "You have 3 new messages",
+  },
+};
+
+
+function App() {
+
+const [lang, setLang] = useState('tr-TR');
+
+  return (
+    <div className="App">
+      <IntlProvider messages={messages[lang]}>
+        <FormattedMessage id="title" />
+        <p>
+          <FormattedMessage id="description" />
+        </p>
+        <br />
+        <button onClick={() => setLang("tr-TR")}>TR</button>
+        <button onClick={() => setLang("en-US")}>EN</button>
+      </IntlProvider>
+    </div>
+  );
+}
+
+export default App

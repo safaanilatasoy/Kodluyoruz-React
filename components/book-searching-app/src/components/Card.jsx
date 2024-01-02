af389bfe7cc2e71150ebd8faddd01a66e36b3275
safaanilatasoy <safaanilatasoy@gmail.com>
2024-01-02 05:35:21 +0300

    main

--- components/book-searching-app/src/components/Card.jsx
+++ components/book-searching-app/src/components/Card.jsx
@@ -1,43 +1,15 @@
 
-import { useState } from "react/cjs/react.development";
-import Modal from "./Modal";
-const Card = ({ book }) => {
-  const [show, setShow] = useState(false);
-  const [bookItem, setItem] = useState();
-  console.log(book);
+const Card = () => {
   return (
-    <>
-      {book.map((item) => {
-        let thumbnail =
-          item.volumeInfo.imageLinks &&
-          item.volumeInfo.imageLinks.smallThumbnail;
-        let amount = item.saleInfo.listPrice && item.saleInfo.listPrice.amount;
-        if (thumbnail != undefined && amount != undefined) {
-          return (
-            <>
-              <div
-                className="card"
-                onClick={() => {
-                  setShow(true);
-                  setItem(item);
-                }}
-              >
-                <img src={thumbnail} alt="" />
-                <div className="bottom">
-                  <h3 className="title">{item.volumeInfo.title}</h3>
-                  <p className="amount">&#8377;{amount}</p>
-                </div>
-              </div>
-              <Modal
-                show={show}
-                item={bookItem}
-                onClose={() => setShow(false)}
-              />
-            </>
-          );
-        }
-      })}
-    </>
-  );
-};
-export default Card;
+    <div>
+      <div className="card">
+        <img src="./images/book.png" alt="" />
+        <div className="bottom">
+            <h3 className="title"></h3>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Card
